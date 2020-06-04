83a361d0af1202e2789529af246d9e31320bbafa
Sean B <butts.sean01@gmail.com>
2020-06-04 08:30:25 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,16 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(phrase="Squawk!")
+<<<<<<< HEAD
   puts "#{phrase}" || "Pretty bird!"
   return "#{phrase}"
   
 end
 parrot
+=======
+  puts "#{phrase}"
+
+end
+parrot("Pretty bird!")
+>>>>>>> 46d8c07ae7747b9b3845461613065572e81dfad6
 
